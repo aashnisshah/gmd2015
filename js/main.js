6631a79e9b8f35e0d73752ed25e164c2258e0b07
Aashni Shah <contact@aashni.me>
2015-01-07 01:44:23 -0500

    display 'main char' on the screen at the top

--- js/main.js
+++ js/main.js
@@ -7,8 +7,27 @@ window.onload=function() {
   context = canvas.getContext("2d");
 };
 
+/* game variables */
+var count = 0;
+var rand = Math.floor((Math.random() * 270) + 1) + 10;
+console.log(rand);
+
+/* character objects */
+var ball = new Object();
+
+ball.posX = rand;
+ball.posY = 0;
+ball.speed = 5;
+ball.img = new Image();
+ball.img.src = "img/sprites/lvl01mainChar.png";
+
+function draw() {
+	context.drawImage(ball.img, ball.posX, ball.posY);
+	console.log('drawn at: ' + ball.posX + ' ' + ball.posY);
+}
+
 function setup() {
-	console.log('setup steps');
+	// console.log('setup steps');
 }
 
 setup();
@@ -16,5 +35,6 @@ var listener = setInterval(main, 1);
 
 /* main function */
 function main() {
-	console.log('loop');
+	// console.log('loop');
+	draw();
 };
