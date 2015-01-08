67241d13547d420277c4a0b68aad281b04279ac1
Aashni Shah <contact@aashni.me>
2015-01-08 02:15:43 -0500

    make character move down the screen

--- js/main.js
+++ js/main.js
@@ -3,31 +3,55 @@ var context;
 var canvas;
 
 window.onload=function() {
-  canvas = document.getElementById("myCanvas");
-  context = canvas.getContext("2d");
 };
 
 /* game variables */
 var count = 0;
-var rand = Math.floor((Math.random() * 270) + 1) + 10;
-console.log(rand);
+var balls = [];
 
 /* character objects */
-var ball = new Object();
 
-ball.posX = rand;
-ball.posY = 0;
-ball.speed = 5;
-ball.img = new Image();
-ball.img.src = "img/sprites/lvl01mainChar.png";
 
 function draw() {
-	context.drawImage(ball.img, ball.posX, ball.posY);
-	console.log('drawn at: ' + ball.posX + ' ' + ball.posY);
+
+	resetScreen();
+	drawBalls();
+	// update position of ball to move it 'down' the screen
+	balls[0].posY = balls[0].posY + balls[0].speed;
+}
+
+function resetScreen() {
+	if(context) {
+		context.fillStyle = "#000000";
+		context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+	} else {
+		console.log('no context');
+	}
+}
+
+function drawBalls() {
+	balls.forEach(function(ball) {
+		context.drawImage(ball.img, ball.posX, ball.posY);
+	})
+}
+
+function createNewBall() {
+	var ball = new Object();
+	var rand = Math.floor((Math.random() * 270) + 1) + 10;
+
+	ball.posX = rand;
+	ball.posY = 0;
+	ball.speed = 0.1;
+	ball.img = new Image();
+	ball.img.src = "img/sprites/lvl01mainChar.png";
+
+	balls.push(ball);
 }
 
 function setup() {
-	// console.log('setup steps');
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+	createNewBall();
 }
 
 setup();
@@ -35,6 +59,5 @@ var listener = setInterval(main, 1);
 
 /* main function */
 function main() {
-	// console.log('loop');
 	draw();
 };
