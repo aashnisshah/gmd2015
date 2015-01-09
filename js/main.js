f0b4cb1cb329f1fe9c054f529919aafc887dac71
Aashni Shah <contact@aashni.me>
2015-01-09 03:02:48 -0500

    Add in wall tiles starting on either left or right hand side

--- js/main.js
+++ js/main.js
@@ -8,14 +8,18 @@ window.onload=function() {
 /* game variables */
 var count = 0;
 var balls = [];
+var wall = [];
+var width = 275;
+var height = 115;
+var blackImage = "img/sprites/lvl01black.png";
 
 /* character objects */
 
 
 function draw() {
-
 	resetScreen();
 	drawBalls();
+	drawWalls();
 	updateBallPosition();
 }
 
@@ -36,7 +40,7 @@ function drawBalls() {
 
 function createNewBall() {
 	var ball = new Object();
-	var rand = Math.floor((Math.random() * 270) + 1) + 10;
+	var rand = getRandomNumber(10, width) + 10;
 
 	ball.posX = rand;
 	ball.posY = 0;
@@ -45,6 +49,50 @@ function createNewBall() {
 	ball.img.src = "img/sprites/lvl01mainChar.png";
 
 	balls.push(ball);
+}
+
+function createWall() {
+	var section = new Object();
+	section.wall = [];
+	section.size = getRandomNumber(1, 5);
+	section.leftOrRight = (getRandomNumber(1, 10) % 2) === 0 ? 0 : width;
+
+	for (var tileCount = 0; tileCount < section.size; tileCount++) {
+		var tile = createNewWallTile(tileCount, section.leftOrRight);
+		section.wall.push(tile);
+	}
+	wall.push(section);
+}
+
+function drawWalls() {
+	for (var wallSize = 0; wallSize < wall.length; wallSize++ ) {
+		for(var sectionSize = 0; sectionSize < wall.length; sectionSize++) {
+			context.drawImage(wall[wallSize].wall[sectionSize].img, wall[wallSize].wall[sectionSize].posX, wall[wallSize].wall[sectionSize].posY);		
+		}
+	}
+}
+
+function updateWallPositions() {
+
+}
+
+function createNewWallTile(tileCount, leftOrRight) {
+	var tile = new Object();
+	var tileOffset = leftOrRight === 0 ? -(10 * tileCount) : 10 * tileCount;
+	tile.posX = leftOrRight + tileOffset;
+	tile.posY = getRandomNumber(10, height) + 10;
+	tile.speed = 0.05;
+	tile.count = tileCount;
+	tile.img = new Image();
+	tile.img.src = "img/sprites/lvl01wall.png";
+
+	return tile;
+}
+
+function getRandomNumber(small, big) {
+	small = small || 0;
+	big = big || 0;
+	return Math.floor(Math.random() * big) + small;
 }
 
 function updateBallPosition() {
@@ -61,6 +109,7 @@ function setup() {
 	canvas = document.getElementById("myCanvas");
 	context = canvas.getContext("2d");
 	createNewBall();
+	createWall();
 }
 
 setup();
