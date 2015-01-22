0819920210adfbf937624b819c772fbd4b1b17a9
Aashni Shah <contact@aashni.me>
2015-01-22 01:21:47 -0500

    correctly identify whether inputted numbers were correct

--- js/leveltwo.js
+++ js/leveltwo.js
@@ -15,10 +15,12 @@ var charY;
 var squares;
 var maxChar;
 var currentChar;
+var inputChar;
 var guesses;
 var blankImage;
 var counter;
 var speed;
+var enterNumbersFlag;
 
 function initializeGlobalVariables() {
 	characterImg = "img/sprites/lvl02mainChar.png";
@@ -28,10 +30,12 @@ function initializeGlobalVariables() {
 	squares = [];
 	maxChar = 5;
 	currentChar = 0;
+	inputChar = 0;
 	guesses = [];
 	blankImage = "img/sprites/lvl01black.png";
 	counter = 0;
-	speed = 350;
+	speed = 150;
+	enterNumbersFlag = false;
 }
 
 function createSquares() {
@@ -53,12 +57,13 @@ function createSquares() {
 						Draw
  *****************************************************/
 function draw() {
-	if(counter === speed && currentChar >= 0) {
-		counter = 0;
-		currentChar = currentChar < maxChar - 1 ? currentChar + 1 : -1;
+	if(currentChar >= 0) {
+		if(counter === speed) {
+			counter = 0;
+			currentChar = currentChar < maxChar - 1 ? currentChar + 1 : -1;
+		}
+		counter = counter + 1;
 	}
-	counter = counter + 1;
-	console.log(counter + ' ' + currentChar);
 	drawCharacter();
 }
 
@@ -78,9 +83,12 @@ function draw() {
  	if(currentChar >= 0) {
 		context.drawImage(patterns[currentChar], squares[currentChar].x, squares[currentChar].y);
  	} else {
- 		// enterNumbers();
- 		console.log('enter numbers');
+ 		enterNumbers();
  	}
+ }
+
+ function enterNumbers() {
+ 	enterNumbersFlag = true;
  }
 
 /*****************************************************
@@ -97,52 +105,44 @@ function getRandomNumber(small, big) {
 					Event Listeners 
  *****************************************************/
 addEventListener("keydown", function (e) {
-  if(e.keyCode == 13){
-  	console.log('enter');
-  	if(guesses.length > 0) {
-  		if (checkNumberIsRight()) {
-  			if(currentChar < maxChar - 1) {
-  				console.log('next character');
-  				patterns[currentChar].src = blankImage;
-  				console.log(patterns[currentChar]);
-  				currentChar = currentChar + 1;
-  			} else if(currentChar === maxChar - 1) {
-  				console.log('game over');
-  			}
-  		}
-  	}
-  	guesses = [];
-  } else if(e.keyCode == 48){
-  	console.log('0');
-  	guesses.push(0);
-  } else if(e.keyCode == 49){
-  	console.log('1');
-  	guesses.push('1');
-  } else if(e.keyCode == 50){
-  	console.log('2');
-  	guesses.push('2');
-  } else if(e.keyCode == 51){
-  	console.log('3');
-  	guesses.push('3');
-  } else if(e.keyCode == 52){
-  	console.log('4');
-  	guesses.push('4');
-  } else if(e.keyCode == 53){
-  	console.log('5');
-  	guesses.push('5');
-  } else if(e.keyCode == 54){
-  	console.log('6');
-  	guesses.push('6');
-  } else if(e.keyCode == 55){
-  	console.log('7');
-  	guesses.push('7');
-  } else if(e.keyCode == 56){
-  	console.log('8');
-  	guesses.push('8');
-  } else if(e.keyCode == 57){
-  	console.log('9');
-  	guesses.push('9');
-  } 
+	if(enterNumbersFlag) {
+	  if(e.keyCode == 13){
+	  	if(guesses.length > 0) {
+	  		if (checkNumberIsRight()) {
+	  			if(inputChar < maxChar - 1) {
+	  				// move on to next image
+	  				patterns[inputChar].src = blankImage;
+	  				inputChar = inputChar + 1;
+	  			} else if(inputChar === maxChar - 1) {
+	  				// you survived
+	  			}
+	  		} else {
+	  			// you got the wrong number
+	  		}
+	  	}
+	  	guesses = [];
+	  } else if(e.keyCode == 48){
+	  	guesses.push(0);
+	  } else if(e.keyCode == 49){
+	  	guesses.push('1');
+	  } else if(e.keyCode == 50){
+	  	guesses.push('2');
+	  } else if(e.keyCode == 51){
+	  	guesses.push('3');
+	  } else if(e.keyCode == 52){
+	  	guesses.push('4');
+	  } else if(e.keyCode == 53){
+	  	guesses.push('5');
+	  } else if(e.keyCode == 54){
+	  	guesses.push('6');
+	  } else if(e.keyCode == 55){
+	  	guesses.push('7');
+	  } else if(e.keyCode == 56){
+	  	guesses.push('8');
+	  } else if(e.keyCode == 57){
+	  	guesses.push('9');
+	  } 
+	}
 
 }, false);
 
@@ -151,8 +151,7 @@ function checkNumberIsRight() {
 	for(var i = 0; i < guesses.length; i++) {
 		number = number + '' + guesses[i];
 	}
-	console.log(number + ' === ' + squares[currentChar].n);
-	return number == squares[currentChar].n;
+	return number == squares[inputChar].n;
 }
 
 /*****************************************************
