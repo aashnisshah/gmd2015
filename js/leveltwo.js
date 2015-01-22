61dd1a8ddc06c59633288b3df3c84b90a582a062
Aashni Shah <contact@aashni.me>
2015-01-21 23:24:29 -0500

    track number inputs and recognize when the correct number has been entered

--- js/leveltwo.js
+++ js/leveltwo.js
@@ -14,6 +14,8 @@ var charX;
 var charY;
 var squares;
 var maxChar;
+var currentChar;
+var guesses;
 
 function initializeGlobalVariables() {
 	characterImg = "img/sprites/lvl02mainChar.png";
@@ -22,15 +24,20 @@ function initializeGlobalVariables() {
 	charY = 100;
 	squares = [];
 	maxChar = 1;
+	currentChar = 0;
+	guesses = [];
 }
 
 function createSquares() {
 	for (var i = 0; i < maxChar; i++) {
-		var coX = charX + (272 * getRandomNumber(0,4));
-		var coY = charY + (156 * getRandomNumber(0,2));
+		var randomX = getRandomNumber(0,4);
+		var randomY = getRandomNumber(0,2);
+		var coX = charX + (272 * randomX);
+		var coY = charY + (156 * randomY);
 		var cordinates = {
 			x: coX,
-			y: coY
+			y: coY,
+			n: (5 * randomY) + randomX + 1
 		};
 		squares.push(cordinates);
 	}
@@ -56,10 +63,10 @@ function draw() {
  }
 
  function drawCharacter() {
- 	for(var i = 0; i < squares.length; i++) {
-		context.drawImage(patterns[0], squares[i].x, squares[i].y);
+ 	// for(var i = 0; i < squares.length; i++) {
+		context.drawImage(patterns[currentChar], squares[currentChar].x, squares[currentChar].y);
 		// debugger	
- 	}
+ 	// }
  }
 
 /*****************************************************
@@ -77,26 +84,60 @@ function getRandomNumber(small, big) {
  *****************************************************/
 addEventListener("keydown", function (e) {
   // up = 38, down = 40
-  if(e.keyCode == 38){
-    balls[CURRENTBALL].speed = balls[CURRENTBALL].speed - 0.05;
-  } else if(e.keyCode == 40){
-    balls[CURRENTBALL].speed = balls[CURRENTBALL].speed + 0.05;
+  if(e.keyCode == 13){
+  	console.log('enter');
+  	if(guesses.length > 0) {
+  		if (checkNumberIsRight()) {
+  			if(currentChar < maxChar - 1) {
+  				console.log('next character');
+  			} else if(currentChar === maxChar - 1) {
+  				console.log('game over');
+  			}
+  		}
+  	}
+  	guesses = [];
+  } else if(e.keyCode == 48){
+  	console.log('0');
+  	guesses.push(0);
+  } else if(e.keyCode == 49){
+  	console.log('1');
+  	guesses.push('1');
+  } else if(e.keyCode == 50){
+  	console.log('2');
+  	guesses.push('2');
+  } else if(e.keyCode == 51){
+  	console.log('3');
+  	guesses.push('3');
+  } else if(e.keyCode == 52){
+  	console.log('4');
+  	guesses.push('4');
+  } else if(e.keyCode == 53){
+  	console.log('5');
+  	guesses.push('5');
+  } else if(e.keyCode == 54){
+  	console.log('6');
+  	guesses.push('6');
+  } else if(e.keyCode == 55){
+  	console.log('7');
+  	guesses.push('7');
+  } else if(e.keyCode == 56){
+  	console.log('8');
+  	guesses.push('8');
+  } else if(e.keyCode == 57){
+  	console.log('9');
+  	guesses.push('9');
   } 
 
 }, false);
 
-// addEventListener('click', function(click) {
-// 	if(click.x >= messageX && click.x <= messageX + 800 && 
-// 		click.y >= messageY && click.y <= messageY + 200 &&
-// 		(stopped || collision || completed)) {
-// 		if(stopped || collision) {
-// 			setup();
-// 		}
-// 		if(completed) {
-// 			alert('completed');
-// 		}
-// 	}
-// }, false);
+function checkNumberIsRight() {
+	var number = '';
+	for(var i = 0; i < guesses.length; i++) {
+		number = number + '' + guesses[i];
+	}
+	console.log(number + ' === ' + squares[currentChar].n);
+	return number == squares[currentChar].n;
+}
 
 /*****************************************************
 					Setup
